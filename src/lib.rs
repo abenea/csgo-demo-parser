e51ea2ea77aa7865077c77b6a271547c88f6363d
Andrei Benea <abenea@gmail.com>
2023-06-04 08:59:08 +0200

    Remove unnecessary use statements.

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,5 @@ pub use parser::DemoParser;
 
 #[cfg(test)]
 mod tests {
-    use crate::parser::DemoParser;
-    use std::fs::File;
-
     pub const DATA_TESTS_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data");
 }
